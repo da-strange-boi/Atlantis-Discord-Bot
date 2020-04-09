7296e8b09fe798834c4fb8f44986a088bed9fb94
Tyler <tylersmaldonado@gmail.com>
2020-04-08 22:38:06 -0400

    fixed help command

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,76 +1,96 @@
 /** @typedef {Eris.Client & getEmbedColor} bot */
 exports.run = async (bot) => {
-  const addCMD = (embed, name, description) => {
-    embed.embed.description += `__**${name}**__ ~ ${description}\n`
-  }
   bot.registerCommand("help", async (message, args) => {
     await bot.checkUserAndGuild(message)
 
-    const helpUserEmbed = {
-      embed: {
-        color: bot.getEmbedColor(bot, message),
-        description: "**Support** me on <:kofi:695186691098411049> **[Ko-fi](https://ko-fi.com/dastrangeboi)  |**  Join Atlantis <:pfp:695187609294471188> **[here](https://discord.gg/Ys9pSZs)**\n\n",
-      }
-    }
-    const helpUtilEmbed = {
-      embed: {
-        color: bot.getEmbedColor(bot, message),
-        description: "",
-        footer: {
-
+    const commandDetails = (name, usage, description, example=false, aliases=false) => {
+      const detailEmbed = {
+        embed: {
+          title: name,
+          description: description,
+          color: bot.getEmbedColor(bot, message),
+          fields: [
+            {
+              name: "Usage",
+              value: usage
+            }
+          ],
+          timestamp: new Date()
         }
       }
+      if (example) {
+        detailEmbed.embed.fields.push({
+          name: "Example",
+          value: example
+        })
+      }
+      if (aliases) {
+        detailEmbed.embed.fields.push({
+          name: "Aliases",
+          value: aliases
+        })
+      }
+      return detailEmbed
     }
-    const helpAdminEmbed = {
-      embed: {
-        color: bot.getEmbedColor(bot, message),
-        description: "",
-        footer: {
 
+    if (!args[0] || args[0] == "admin") {
+      const helpEmbed = {
+        embed: {
+          title: "Atlantis Help",
+          color: bot.getEmbedColor(bot, message),
+          description: `This is the full command list for ${bot.user.username}! For more info on what a command does do \`a!help <command name>\`\nThanks to \`pri8000#8266\` for the profile picture art!`,
+          fields: [
+            {
+              name: "General",
+              value: "`hunt`, `battle`, `owo`, `praycurse`, `huntbot`, `show`"
+            },
+            {
+              name: "Util",
+              value: "`atlantis`, `invite`"
+            },
+            {
+              name: "Admin (must be admin to use)",
+              value: "`deluser`, `delbot`, `owochannel`, `welcome`"
+            }
+          ],
+          footer: {
+            text: "Atlantis Bot, made by `da strange boi#7087` with lots of love!"
+          },
+          timestamp: new Date()
         }
       }
+
+      if (bot.admins.includes(message.author.id) && args[0] == "admin") {
+        helpEmbed.embed.fields.push({name: "Bot Admin", value: "`status`, `servers`"})
+      }
+      if (message.author.id == "295255543596187650" && args[0] == "admin") {
+        helpEmbed.embed.fields.push({name: "Bot Owner", value: "`eval`"})
+      }
+
+      bot.createMessage(message.channel.id, helpEmbed)
     }
-    // User Command
-    addCMD(helpUserEmbed, "hunt", "Toggles the reminder for \`owo hunt\`")
-    addCMD(helpUserEmbed, "battle", "Toggles the reminder for \`owo battle\`")
-    addCMD(helpUserEmbed, "owo", "Toggles the reminder for \`owo\`")
-    addCMD(helpUserEmbed, "praycurse", "Toggles the reminder for \`owo pray\` & \`owo curse\`")
-    addCMD(helpUserEmbed, "huntbot", "Toggles the reminder for \`owo huntbot\`")
-    addCMD(helpUserEmbed, "show", "Shows what is enabled/disabled")
-    
-
-    // Util Commands
-    helpUtilEmbed.embed.description += "\n***Until Commands***\n\n"
-    addCMD(helpUtilEmbed, "atlantis", "Sends an invite link to join Atlantis")
-    addCMD(helpUtilEmbed, "invite", "Invite Atlantis bot to your server")
-
-    // Admin Commands
-    if (message.member.permission.has("administrator")) {
-
-      helpAdminEmbed.embed.footer.text = "Atlantis Bot, made by `da strange boi#7087` with lots of love!"
-      helpAdminEmbed.embed.timestamp = new Date()
-
-      helpAdminEmbed.embed.description += "\n***Admin Commands***\n(run the command as is for more info)\n\n"
-      addCMD(helpAdminEmbed, "message", "Shows what channels are in what category below")
-      addCMD(helpAdminEmbed, "deleteusermessages", "Deletes user messages in a given channel")
-      addCMD(helpAdminEmbed, "deletebotmessages", "Deletes bot messages in a given channel")
-      addCMD(helpAdminEmbed, "owochannel", "Deletes all messages expect \"owo\"")
-      addCMD(helpAdminEmbed, "welcomechannel", "Display a welcome card to new members in a given channel")
-    
-      helpAdminEmbed.embed.description += "\nThanks to `pri8000#8266` for the profile picture art!"
-    } else {
-      helpUtilEmbed.embed.description += "\nThanks to `pri8000#8266` for the profile picture art!"
-      helpUtilEmbed.embed.footer.text = "Atlantis Bot, made by `da strange boi#7087` with lots of love!"
-      helpUtilEmbed.embed.timestamp = new Date()
+    if (args[0]) {
+      if (args[0].toLowerCase() == "admin") return
+      // User wants more detail on command (a!help hunt)
+      let commandDetailToSend
+      switch (args[0].toLowerCase()) {
+        case "hunt": commandDetailToSend = commandDetails("Hunt", "`a!hunt`", "Toggles the reminder for \`owo hunt\`", false, "`h`, `catch`"); break
+        case "battle": commandDetailToSend = commandDetails("Battle", "`a!battle`", "Toggles the reminder for \`owo battle\`", false, "`b`, `fight`"); break
+        case "owo": commandDetailToSend = commandDetails("OwO", "`a!owo`", "Toggles the reminder for \`owo\`"); break
+        case "praycurse": commandDetailToSend = commandDetails("Praycuse", "`a!praycurse`", "Toggles the reminder for \`owo pray\` & \`owo curse\`", false, "`pray`, `curse`"); break
+        case "huntbot": commandDetailToSend = commandDetails("Huntbot", "`a!huntbot`", "Toggles the reminder for \`owo huntbot\`", false, "`huntbot`"); break
+        case "show": commandDetailToSend = commandDetails("Show", "`a!show`", "Shows what is enabled/disabled"); break
+        case "atlantis": commandDetailToSend = commandDetails("Atlantis", "a!atlantis", "Sends an DM invite link to join Atlantis", false, "`atl`"); break
+        case "invite": commandDetailToSend = commandDetails("Invite", "`a!invite`", "Invite Atlantis bot to your server"); break
+        case "deluser": commandDetailToSend = commandDetails("Deluser", "`a!deluser add/delete <#channel mention>`", "Deletes user messages in a given channel", "`a!deluser add #general`"); break
+        case "delbot": commandDetailToSend = commandDetails("Delbot", "`a!delbot add/delete <#channel mention>`", "Deletes bot messages in a given channel", "`a!delbot delete #owo`"); break
+        case "owochannel": commandDetailToSend = commandDetails("OwO Channel", "`a!owochannel add/delete <#channel mention>`", "Deletes all messages expect \"owo\"", "`a!owochannel add #owo`"); break
+        case "welcome": commandDetailToSend = commandDetails("Welcome", "`a!welcome add/delete <#channel mention>`", "Display a welcome card to new members in a given channel", "`a!welcome add #welcome`\n`a!welcome text Welcome {user} to **{server}**!`"); break
+        default: commandDetailToSend = {embed:{title:"Error",color:bot.color.red,description:"You have to add specify a command name",timestamp: new Date()}}; break
+      }
+      bot.createMessage(message.channel.id, commandDetailToSend)
     }
 
-    bot.createMessage(message.channel.id, helpUserEmbed).then(() => {
-      bot.createMessage(message.channel.id, helpUtilEmbed).then(() => {
-        if (message.member.permission.has("administrator")) {
-          bot.createMessage(message.channel.id, helpAdminEmbed)
-        }
-      })
-    })
   }, {
     cooldown: 3000,
     cooldownMessage: "Whoa there slow down, the cooldown is 3 seconds!"
