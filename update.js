d0c0d58d6f66230de0e5aeae7e3212946a0f01ba
Tyler <tylersmaldonado@gmail.com>
2020-04-10 19:48:40 -0400

    fuck

--- update.js
+++ update.js
@@ -7,8 +7,10 @@ client.connect("mongodb://localhost:27017", { useUnifiedTopology: true }, (err, 
   await userdata.find({}).toArray((err, user) => {
     if (err) throw err
 
-    if (!user.stats) {
-      await userdata.findOneAndUpdate({ userID: user.userID }, {$set: {"stats":{owoCount: 0,huntCount: 0,battleCount: 0,praycurseCount: 0,completedHuntbots: 0,totalHuntbotTime: 0,dailyOwoCount: 0,dailyHuntCount: 0,dailyPraycurseCount: 0}}})
-    }
+    user.forEach(us => {
+      if (!us.stats) {
+        await userdata.findOneAndUpdate({ userID: us.userID }, {$set: {"stats":{owoCount: 0,huntCount: 0,battleCount: 0,praycurseCount: 0,completedHuntbots: 0,totalHuntbotTime: 0,dailyOwoCount: 0,dailyHuntCount: 0,dailyPraycurseCount: 0}}})
+      }
+    })
   })
 })
