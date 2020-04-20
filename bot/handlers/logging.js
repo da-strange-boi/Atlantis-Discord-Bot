1d4ac4e78386bf6d78c7731cdd7adb31ad2de001
Tyler <tylersmaldonado@gmail.com>
2020-04-20 12:15:13 -0400

    updated loggin style

--- bot/handlers/logging.js
+++ bot/handlers/logging.js
@@ -3,7 +3,12 @@ const version = require("../../package.json")
 
 const getCurrentDate = () => {
   const date = new Date()
-  return `${date.getHours()}:${date.getMinutes()} ${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`
+  const formattedTime = `${date.getHours()}:${date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`} ${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`
+  return `${chalk.yellow("[") + chalk.blue(formattedTime) + chalk.yellow("]")}`
+}
+
+const getContentFormat = (message) => {
+  return `${chalk.yellow(" [") + message + chalk.yellow("] ")}`
 }
 
 /**
@@ -13,13 +18,13 @@ const getCurrentDate = () => {
  */
 const logging = async function Log (logType, logMessage) {
   switch (logType) {
-    case 'system': console.log(chalk.inverse('[LOG][SYSTEM]') + chalk.hex("#FFFFFF")(` ${logMessage} `) + chalk.blue(getCurrentDate())); break
-    case 'error': console.log(chalk.inverse('[LOG][ERROR]') + chalk.hex("#FF0000")(` ${logMessage} `) + chalk.blue(getCurrentDate())); break
+    case 'system': console.log(chalk.inverse('[LOG][SYSTEM]') + getContentFormat(chalk.hex("#FFFFFF")(logMessage)) + getCurrentDate()); break
+    case 'error': console.log(chalk.inverse('[LOG][ERROR]') + chalk.hex("#FF0000")(logMessage) + getCurrentDate()); break
     // case 'statsPosted': console.log(chalk.inverse('[LOG][STATS POSTED]') + chalk.hex(config.color.lightblue)(` ${logMessage} `) + chalk.blue(getCurrentDate())); break
-    case 'botOnline': console.log(chalk.inverse('[LOG][ONLINE]') + chalk.hex("#00FFFF")(` Version ${version.version} `) + chalk.blue(getCurrentDate())); break
-    case 'dbConnected': console.log(chalk.inverse('[LOG][SYSTEM]') + chalk.hex("#FFAA00")(' Database Connected Successfully ') + chalk.blue(getCurrentDate())); break
-    case 'botDisconnected': console.log(chalk.inverse('[LOG][DISCONNECT]') + chalk.hex("#00FF00")(' Bot Disconnected ') + chalk.blue(getCurrentDate())); break
-    default: console.log(chalk.inverse('[LOG][DEFAULT]') + chalk.hex("#FFFFFF")(` ${logMessage} `) + chalk.blue(getCurrentDate())); break
+    case 'botOnline': console.log(chalk.inverse('[LOG][ONLINE]') + chalk.hex("#00FFFF")(` Version ${version.version} `) + getCurrentDate()); break
+    case 'dbConnected': console.log(chalk.inverse('[LOG][SYSTEM]') + chalk.hex("#FFAA00")(' Database Connected Successfully ') + getCurrentDate()); break
+    case 'botDisconnected': console.log(chalk.inverse('[LOG][DISCONNECT]') + chalk.hex("#00FF00")(' Bot Disconnected ') + getCurrentDate()); break
+    default: console.log(chalk.inverse('[LOG][DEFAULT]') + getContentFormat(chalk.hex("#FFFFFF")(logMessage)) + getCurrentDate()); break
   }
 }
 module.exports = logging
