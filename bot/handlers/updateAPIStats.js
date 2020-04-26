62e7f9f2a0b9467599531f91c1792424d51e9836
Tyler <tylersmaldonado@gmail.com>
2020-04-26 00:17:48 -0400

    eh

--- bot/handlers/updateAPIStats.js
+++ bot/handlers/updateAPIStats.js
@@ -5,19 +5,25 @@ module.exports = async (bot) => {
   const dbl = new DBL(process.env.DBL_TOKEN, {webhookPort: process.env.WEBHOOK_PORT, webhookAuth: process.env.WEBHOOK_AUTH})
   const bls = new BLS.Client({id: bot.user.id, botToken: process.env.BLS_TOKEN})
 
+  console.log("log1")
+
   dbl.on("error", (e) => {
     bot.log("error", e)
   })
 
   dbl.webhook.on("ready", hook => {
     console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`)
+    console.log("log2")
 
     // when user votes
-    dbl.webhook.on("vote", vote => {
+    dbl.webhook.on("vote", (vote) => {
+      console.log("log3")
       bot.database.Userdata.findOne({ userID: vote.id }, async (err, userdata) => {
+        console.log("log4")
         if (err) bot.log("error", err)
 
         if (userdata) {
+          console.log("log5")
           let modifiedCustom = userdata.customs
           for (let i = 0; i < modifiedCustom.length; i++) {
             modifiedCustom[i].unlocked = true
