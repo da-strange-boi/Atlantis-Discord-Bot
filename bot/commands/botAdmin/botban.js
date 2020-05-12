c6a9394ce3fdbf8f253fb0434fa35c6ca1f929d6
Tyler <tylersmaldonado@gmail.com>
2020-05-11 20:54:35 -0400

    added bot admin cmd

--- bot/commands/botAdmin/botban.js
+++ bot/commands/botAdmin/botban.js
@@ -23,29 +23,53 @@ exports.run = async (bot) => {
           await bot.createMessage(message.channel.id, botbannedEmbed)
         } else {
 
-          const userID = message.mentions[0].id || args[0]
-          const reason = args.splice(1, args.length-1).join(" ")
-          const userObj = await bot.getRESTUser(userID)
+          if (args[0] == "delete") {
 
-          await bot.database.BotBan.insertOne({
-            userID: userID,
-            name: `${userObj.username}#${userObj.discriminator}`,
-            reason: reason
-          })
+            const userID = message.mentions[0].id || args[1]
+            const userObj = await bot.getRESTUser(userID)
 
-          const botbannedEmbed = {
-            embed: {
-              author: {
-                name: `${userObj.username}#${userObj.discriminator} had been bot banned!`,
-                icon_url: `${userObj.avatarURL}`
-              },
-              color: bot.color.red,
-              description: `<@${userID}>\nReason: \`${reason}\``,
-              timestamp: new Date()
+            await bot.database.BotBan.deleteOne({ userID: userID })
+
+            const botunbannedEmbed = {
+              embed: {
+                author: {
+                  name: `${userObj.username}#${userObj.discriminator} had been bot unbanned!`,
+                  icon_url: `${userObj.avatarURL}`
+                },
+                color: bot.color.green,
+                timestamp: new Date()
+              }
             }
+            _.remove(bot.botBannedUsers, (n) => {
+              return n == userID
+            })
+            await bot.createMessage(message.channel.id, botunbannedEmbed)
+
+          } else {
+            const userID = message.mentions[0].id || args[0]
+            const reason = args.splice(1, args.length-1).join(" ")
+            const userObj = await bot.getRESTUser(userID)
+
+            await bot.database.BotBan.insertOne({
+              userID: userID,
+              name: `${userObj.username}#${userObj.discriminator}`,
+              reason: reason
+            })
+
+            const botbannedEmbed = {
+              embed: {
+                author: {
+                  name: `${userObj.username}#${userObj.discriminator} had been bot banned!`,
+                  icon_url: `${userObj.avatarURL}`
+                },
+                color: bot.color.red,
+                description: `<@${userID}>\nReason: \`${reason}\``,
+                timestamp: new Date()
+              }
+            }
+            bot.botBannedUsers.push(userID)
+            await bot.createMessage(message.channel.id, botbannedEmbed)
           }
-          bot.botBannedUsers.push(userID)
-          await bot.createMessage(message.channel.id, botbannedEmbed)
         }
       })
     }
