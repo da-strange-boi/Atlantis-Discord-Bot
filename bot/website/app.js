53da45975730a72425c945f0b54469326bd86845
Tyler <tylersmaldonado@gmail.com>
2020-05-15 08:54:32 -0400

    more work on website

--- bot/website/app.js
+++ bot/website/app.js
@@ -2,6 +2,8 @@ const express = require("express")
 const exphbs = require("express-handlebars")
 const path = require("path")
 
+const util = require('util')
+
 exports.run = async (bot) => {
 
   const app = express()
@@ -26,7 +28,34 @@ exports.run = async (bot) => {
   app.get("/members", async (req, res) => {
     res.render("members", {
       title: "Atlantis Members",
-      cssFileName: "members"
+      cssFileName: "members",
+      members: await bot.database.Website.find({}).toArray(),
+      helpers: {
+        getPfp: function(obj) {return `https://cdn.discordapp.com/avatars/${obj.userID}/${obj.avatar.startsWith("a_") ? `${obj.avatar}.gif` : `${obj.avatar}.png`}`},
+        getLevel: function(obj) {return obj.level == 3 ? "Atlantis Admin/Mod" : obj.level == 1 ? "Atlantis Recruit" : "Atlantis Member"},
+        getLevelColor: function(obj) {return obj.level == 3 ? "level blue" : "level gold"},
+        getBoost: function(obj) {return obj.premium != null ? `/images/server_boost.png` : `/images/blank.png`},
+        hitreqText: function(obj) {return obj.owosPastWeek >= 50 ? `✔️ Hit owo requirement` : `❌ Did not hit owo requirement`},
+        hitreqColor: function(obj) {return obj.owosPastWeek >= 50 ? "green" : "red"},
+        getOwoCount: function(obj) {return `${obj.owosPastWeek} owos`}
+      }
+    })
+  })
+
+  // Look at a person
+  app.get("/member/:userID", async (req, res) => {
+    res.render("member", {
+      title: "Member View",
+      cssFileName: "memberView",
+      member: await bot.database.Website.find({ userID: req.params.userID }).toArray(),
+      helpers: {
+        getPfp: function(obj) {obj = obj[0];return `https://cdn.discordapp.com/avatars/${obj.userID}/${obj.avatar.startsWith("a_") ? `${obj.avatar}.gif` : `${obj.avatar}.png`}`},
+        getLevel: function(obj) {obj = obj[0];return obj.level == 3 ? "Atlantis Admin/Mod" : obj.level == 1 ? "Atlantis Recruit" : "Atlantis Member"},
+        getLevelColor: function(obj) {obj = obj[0];return obj.level == 3 ? "level blue" : "level gold"},
+        getBoost: function(obj) {obj = obj[0];return obj.premium != null ? `/images/server_boost.png` : `/images/blank.png`},
+        hitreqText: function(obj) {obj = obj[0];return obj.owosPastWeek >= 50 ? "✔️ Hit owo requirement" : "❌ Did not hit owo requirement"},
+        hitreqColor: function(obj) {obj = obj[0];return obj.owosPastWeek >= 50 ? "green" : "red"}
+      }
     })
   })
 
