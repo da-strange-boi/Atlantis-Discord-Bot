c0740d935a5ca2b00426e8d12b8905851e3cea00
Tyler <tylersmaldonado@gmail.com>
2020-06-05 19:02:14 -0400

    yep

--- bot/commands/util/atlantis.js
+++ bot/commands/util/atlantis.js
@@ -3,14 +3,19 @@ exports.run = async (bot) => {
     await bot.checkUserAndGuild(message)
     if (bot.checkBannedUsers(message.author.id)) return
     
+    const getAtlantisInvite = process.env.ATLANTIS_INVITE
 
-    try {
-      await bot.getDMChannel(message.author.id).then(async channel => {
-        await channel.createMessage("https://discord.gg/FCUZeGb")
-      })
-      await bot.createMessage(message.channel.id, "A DM has been sent with the invite link to Atlantis :trident:")
-    } catch (e) {
-      bot.createMessage(message.channel.id, `${bot.user.username} could not DM you\nhttps://discord.gg/FCUZeGb`)
+    if (getAtlantisInvite !== "null") {
+      try {
+        await bot.getDMChannel(message.author.id).then(async channel => {
+          await channel.createMessage(getAtlantisInvite)
+        })
+        await bot.createMessage(message.channel.id, "A DM has been sent with the invite link to Atlantis :trident:")
+      } catch (e) {
+        bot.createMessage(message.channel.id, `${bot.user.username} could not DM you\n${getAtlantisInvite}`)
+      }
+    } else {
+      await bot.createMessage(message.channel.id, "Sorry this command is currently disabled")
     }
   }, {
     aliases: ["atl"],
